7cf14a19b56456cdea517881000aa6b5bfd1e0d6
ITesserakt <potryas85@mail.ru>
2021-11-09 00:43:25 +0300

    Client interface

--- backend/project/include/Client.h
+++ backend/project/include/Client.h
@@ -4,5 +4,23 @@
 
 #pragma once
 
+#include <memory>
+
+#include "Serializer.h"
+
+using IStream = std::istringstream;
+using OStream = std::ostringstream;
+
+template <Serializer<IStream, OStream> S>
 class Client {
+private:
+    std::unique_ptr<S> serializer;
+
+public:
+    template <typename... Args>
+    void createSerializer(Args &&...args) {
+        serializer = std::make_unique<S>(std::forward<Args>(args)...);
+    }
+
+    virtual void receive(std::string data) = 0;
 };
